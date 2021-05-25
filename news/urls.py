a80606b353f816d175ac45b9464621f4312b1719
sindetisarah <sindetisara@gmail.com>
2021-05-25 21:07:27 +0300

    accessed th day of the week for each particular news

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.welcome,name="welcome"),
-    path('news_of_day',views.news_of_day,name="news_of_day")
+    path('news_of_day',views.news_of_day,name="news_of_day"),
+    path('past_days_news\d{4}-d{2}-\d{2}',views.past_days_news,name="past_days_news")
 
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime as dt
@@ -8,18 +9,28 @@ def welcome(request):
 def news_of_day(request):
     date=dt.date.today()
     #calling the convert day function
-    # day=convert_dates(date)
+    day=convert_dates(date)
     html=f'''
         <html>
         <body>
-        <h1>News for  {date.day}-{date.month}-{date.year}</h1></body>
+        <h1>News for {day} {date.day}-{date.month}-{date.year}</h1></body>
         </html>
 
 
     '''
     return HttpResponse(html)
-# def convert_dates(date):
-#     day_number=dt.date.weekday
-#     days=['Monday','Tuesday','Wednesday','Thursday','Friday']
-#     day=days[day_number]
-#     return day
+    # //getting the day of the week
+    
+def convert_dates(dates):
+    day_number=dt.date.weekday(dates)
+    days=['Monday','Tuesday','Wednesday','Thursday','Friday']
+    #returning the actual days of the week
+    day=days[day_number]
+    return day
+#A function to capture a particular day the news was issued
+def past_days_news(request,past_date):
+    try:
+        #strptime function passes in date as a string and is late coveted to a date object by the date function
+        date=dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        raise Http404()
